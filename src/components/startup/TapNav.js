f1924cad65f03abc5d06a4291f4a323c32cfd3d6
imran <64401656+impactmadeimran@users.noreply.github.com>
2020-12-02 13:04:00 +0000

    fixed footer

--- src/components/startup/TapNav.js
+++ src/components/startup/TapNav.js
@@ -9,7 +9,7 @@ const TapNav = () => {
             <div className = 'menu'>
             </div>
             <nav>
-                    <img className = 'nav-icon' src = {Vector} alt= 'icon' />
+                    <img  className = 'nav-icon' src = {Vector} alt= 'icon' />
                 <ul className = 'nav-links'>
                     <li><Link to = '/whyus'>WHY US?  </Link></li>
                     <li><Link to = '/tapabout'>ABOUT  </Link></li>
