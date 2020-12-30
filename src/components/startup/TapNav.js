485fce1c012bb5f938ee353bf10e6bd9109ef732
imran <64401656+impactmadeimran@users.noreply.github.com>
2020-12-30 21:50:22 +0000

    media queries

--- src/components/startup/TapNav.js
+++ src/components/startup/TapNav.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import {NavLink} from 'react-router-dom'
 import {CgLogIn} from 'react-icons/cg'
 import {BsCloudDownload} from 'react-icons/bs'
 import Vector from './images/Vector.jpg'
@@ -11,11 +11,11 @@ const TapNav = () => {
             <nav>
                     <img  className = 'nav-icon' src = {Vector} alt= 'icon' />
                 <ul className = 'nav-links'>
-                    <li><Link to = '/whyus'>WHY US?  </Link></li>
-                    <li><Link to = '/tapabout'>ABOUT  </Link></li>
-                    <li><Link to = '/taplogin'>LOGIN <CgLogIn /> </Link></li>
-                    <li><Link to = '/tapsignup'>SIGN UP</Link></li>
-                    <li><Link to = '/' >DOWNLOAD <BsCloudDownload /> </Link></li>
+                    <li><NavLink to = '/whyus'>WHY US?  </NavLink></li>
+                    <li><NavLink to = '/tapabout'>ABOUT  </NavLink></li>
+                    <li><NavLink to = '/taplogin'>LOGIN <CgLogIn /> </NavLink></li>
+                    <li><NavLink to = '/tapsignup'>SIGN UP</NavLink></li>
+                    <li><NavLink to = '/' >DOWNLOAD <BsCloudDownload /> </NavLink></li>
 
                 </ul>
             </nav> 
